d6babc7bb691702420e686703f84450a1383596e
Jonathan Washington <jonathan.north.washington@gmail.com>
2015-01-17 03:01:49 -0500

    Basic Yiddish layout, and basic installer script

---
+++ xkb/install.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import os, shutil
+from lxml import etree
+
+lg = sys.argv[1]
+xkbdir = "/usr/share/X11/xkb"
+
+layout = etree.fromstring("\
+	<layout>\
+		<configItem>\
+			<name>{}</name>\
+			<shortDescription>{}</shortDescription>\
+			<description>{}</description>\
+			<languageList>\
+				<iso639Id>{}</iso639Id>\
+			</languageList>\
+		</configItem>\
+	</layout>".format(lg, lg, lg, lg))
+
+shutil.copyfile(lg, xkbdir+"/symbols/"+lg)
+
+root=etree.parse(xkbdir+"/rules/evdev.xml")
+
+#xkbConfigRegistry → layoutList
+#cr = root.find("xkbConfigRegistry")
+#ll = cr.find("layoutList")
+ll = root.find(".//layoutList")
+ll.append(layout)
+root.write(xkbdir+"/rules/evdev.xml", pretty_print=True)
+
